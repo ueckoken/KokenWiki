6d9669fc799dab62d4207ae3758eeed6d1366d7c
otariidae <otariidae@users.noreply.github.com>
2020-02-02 07:03:55 +0900

    use monaco editor instead of textarea

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+
+environment.plugins.prepend(
+  'MonacoEditor',
+  new MonacoWebpackPlugin({
+    languages: ['markdown']
+  })
+)
 
 module.exports = environment
