c1d06bf3755827dd70337665b909d1d269b387dc
otariidae <otariidae@users.noreply.github.com>
2020-05-28 08:50:45 +0900

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -153,6 +153,6 @@ Rails.application.routes.draw do
   # devise_for :users
   # resources :user_groups
   # resources :pages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: 'pages#index'
 end
