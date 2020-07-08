08c2f8826a697f5d2023471ec0c08d4d6b3a7a8a
otariidae <otariidae@users.noreply.github.com>
2020-07-09 02:58:10 +0900

    set SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:production_smtp, :user_name),
+    password: Rails.application.credentials.dig(:production_smtp, :password),
+    domain: Rails.application.credentials.dig(:production_smtp, :domain),
+    address: Rails.application.credentials.dig(:production_smtp, :address),
+    port: Rails.application.credentials.dig(:production_smtp, :port),
+    authentication: Rails.application.credentials.dig(:production_smtp, :authentication),
+    enable_starttls_auto: Rails.application.credentials.dig(:production_smtp, :enable_starttls_auto)
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
