01cdf82bd79ff4d08d065aabb5856f2484533bfc
otariidae <otariidae@users.noreply.github.com>
2021-03-07 17:26:27 +0900

    disable colorize logging for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,6 +105,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Do not dump ANSI color codes
+  config.colorize_logging = false
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
