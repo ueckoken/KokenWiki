eb1f7ac55fa96fbb6f2eedf3cf5da307a070ef87
otariidae <otariidae@users.noreply.github.com>
2021-04-20 21:43:06 +0900

    do not initially create databases; use rails db:create instead to respect config/database.yml

--- init-mysql-docker-compose.sql
+++ init-mysql-docker-compose.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE IF NOT EXISTS `wiki1_development`;
-CREATE DATABASE IF NOT EXISTS `wiki1_test`;
-
 CREATE USER 'rails'@'%' IDENTIFIED BY 'rails';
 GRANT ALL ON `wiki1_development`.* TO 'rails'@'%';
 GRANT ALL ON `wiki1_test`.* TO 'rails'@'%';
