a302fe2c89833db0da0e7cf928d23f967333abd6
otariidae <otariidae@users.noreply.github.com>
2021-07-31 07:25:41 +0900

    use has_secure_token (#167)

--- app/models/invitation_token.rb
+++ app/models/invitation_token.rb
@@ -1,9 +1,6 @@
 class InvitationToken < ApplicationRecord
   belongs_to :created_by, class_name: "User"
-
-  validates :token, presence: true, length: { minimum: 16, maximum: 1024 }
-  validates :token, uniqueness: true
-  validates :token, format: { without: URI::UNSAFE }
+  has_secure_token length: 36
 
   def registration_path
     return Rails.application.routes.url_helpers.new_user_registration_path(t: token)
