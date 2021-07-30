36cb8a10b9907805ecd6fbe8187424e1b5833a07
otariidae <otariidae@users.noreply.github.com>
2021-07-31 06:51:18 +0900

    image lazyload enabled by default (#170)

--- app/javascript/components/Markdown.tsx
+++ app/javascript/components/Markdown.tsx
@@ -1,6 +1,34 @@
 import React, { memo, useRef, useEffect, useState, FC } from "react"
 import Viewer from "@toast-ui/editor/dist/toastui-editor-viewer"
 import customHTMLSanitizer from "./html-sanitizer-for-tui-editor"
+import { CustomHTMLRenderer } from "@toast-ui/editor"
+
+const customImageRenderer: CustomHTMLRenderer = (_, { origin }) => {
+    const result = origin!()
+    if (result === null) {
+        return null
+    }
+    if (result instanceof Array) {
+        for (const token of result) {
+            if (token.type !== "openTag") {
+                continue
+            }
+            if (token.attributes === undefined) {
+                token.attributes = {}
+            }
+            token.attributes.loading = "lazy"
+        }
+        return result
+    }
+    if (result.type !== "openTag") {
+        return result
+    }
+    if (result.attributes === undefined) {
+        result.attributes = {}
+    }
+    result.attributes.loading = "lazy"
+    return result
+}
 
 type Props = {
     markdown: string
@@ -17,6 +45,11 @@ const Markdown: FC<Props> = ({ markdown }) => {
             el: rootEl.current,
             initialValue: markdown,
             customHTMLSanitizer: customHTMLSanitizer,
+            // type for customHTMLRenderer is wrong
+            // ref: https://github.com/nhn/tui.editor/issues/1064
+            customHTMLRenderer: {
+                image: customImageRenderer
+            } as any
         })
         setViewerInst(inst)
         return () => {
