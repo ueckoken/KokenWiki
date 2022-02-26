49a6dafc54317dcec63dcc4075071e5587bd6284
otariidae <otariidae@users.noreply.github.com>
2022-02-27 06:16:44 +0900

    add stricter search mode

--- app/models/page_search_form.rb
+++ app/models/page_search_form.rb
@@ -6,11 +6,13 @@ class PageSearchForm
   attribute :period, :integer, default: -1
   attribute :order, :string, default: :best_match
   attribute :target, :string, default: :content
+  attribute :mode, :string, default: :natural_language
   alias_attribute :query, :q
 
   validates :period, inclusion: { in: [-1, 1.week, 1.month, 1.year]  }
   validates :order, inclusion: { in: %w(best_match updated_at_asc updated_at_desc) }
   validates :target, inclusion: { in: %w(content title) }
+  validates :mode, inclusion: { in: %w(natural_language slower_stricter) }
 
   def period_duration
     return ActiveSupport::Duration.build(period)
