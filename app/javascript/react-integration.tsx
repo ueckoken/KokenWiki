5aeef2c4031e96a2fd4f40917e598e437946a9d8
otariidae <otariidae@users.noreply.github.com>
2022-03-07 03:26:43 +0900

    fix dynamic imports
    add webpack magic comment to generate no extra chunk in dynamic import

--- app/javascript/react-integration.tsx
+++ app/javascript/react-integration.tsx
@@ -6,8 +6,16 @@ async function mountReactComponent(entrypointElement: HTMLElement) {
     if (componentName === undefined) {
         return
     }
+    // 1. webpack generates chunk files
+    // 2. asset pipeline generates digests for chunk files
+    // 3. application.js tries to import non-digest chunk files at runtime and fails to do it
+    // workaround: do not generate chunk files
+    // see https://webpack.js.org/api/module-methods/ for webpack magic comments
     const Component: ComponentType = (
-        await import(`./components/${componentName}`)
+        await import(
+            /* webpackMode: "eager" */
+            `./components/${componentName}`
+        )
     ).default
     const rawProps = entrypointElement.dataset.props
     const params = rawProps === undefined ? {} : JSON.parse(rawProps)
