9b344bc728cb7567db89855d04547f514cdf6459
gotti <18141824+gotti@users.noreply.github.com>
2025-05-15 00:41:20 +0900

    add deploy action
    Signed-off-by: gotti <18141824+gotti@users.noreply.github.com>

--- tf/af.tf
+++ tf/af.tf
@@ -13,3 +13,11 @@ resource "google_artifact_registry_repository_iam_member" "kokenwiki" {
   role       = "roles/artifactregistry.admin"
   member     = "serviceAccount:${google_service_account.kokenwiki-cloudrun.email}"
 }
+
+resource "google_artifact_registry_repository_iam_member" "kokenwiki-deployer" {
+  project    = var.project_id
+  location   = var.region
+  repository = google_artifact_registry_repository.kokenwiki.name
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:${google_service_account.kokenwiki-deployer.email}"
+}
